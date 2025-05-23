fdb7c14bb715fe61066a0bca96cd19a4cc13bb4f
Eswar2585 <eswar20005@gmail.com>
2025-05-23 09:46:07 +0530

    Update useController.js

--- controller/useController.js
+++ controller/useController.js
@@ -1,5 +1,5 @@
 
-// export const register=async(req,res)=>{
-//     const {name}=req.body;
-//     res.status(200).json({data: `welcome ${name}`})
-// }
+export const register=async(req,res)=>{
+    const {name}=req.body;
+    res.status(200).json({data: `welcome ${name}`})
+}
